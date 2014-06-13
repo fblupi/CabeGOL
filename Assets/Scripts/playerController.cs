dec607d0b60e848e8b744b9447d60f9fd3b9aae5
fblupi <fboli94@gmail.com>
2014-06-13 20:08:13 +0200

    Primer commit
    Un jugador, una pelota que bota y... bueno, poco más.

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+	//Velocidad máxima a la que se desplazará la nave.
+	public float maxSpeed = 5f;
+	
+	//FixedUpdate es la función recomendada en la que hacer cambios que afecten al motor de física
+	void FixedUpdate()
+	{
+		//Obtenemos los valores de nuestro "input" (-1.0f -> 1.0f)
+		float moveX = Input.GetAxis ("Horizontal");
+		
+		//Proporcionamos estos valores al rigidbody2D como velocidad
+		rigidbody2D.velocity = new Vector2 (moveX * maxSpeed, 0);
+	}
+}
